f9ff3dd35971d9222c7886be693e72470780c185
hubo00 <hub.bukowski@gmail.com>
2021-10-09 18:21:18 +0100

    Implemented sorting algorithm & printing out top players

--- players/Source.cpp
+++ players/Source.cpp
@@ -1,10 +1,30 @@
+// Hubert Bukowski | x00161897
+
+/*
+USAGE:			Modified
+TITLE:			Reading and Processing a File Line by Line in C++
+AUTHOR:			Q A
+DATE:			24/MAR/2018
+CODE VERSION:	N/A
+AVAILABILITY:	https://www.systutorials.com/how-to-process-a-file-line-by-line-in-c/
+*/
+
+/*
+USAGE:			Modified
+TITLE:			C++ program to sort an array in Descending Order
+AUTHOR:			IncludeHelp.com
+DATE:			N/A
+CODE VERSION:	N/A
+AVAILABILITY:	https://www.includehelp.com/cpp-programs/sort-an-array-in-descending-order.aspx
+*/
 #include <iostream>
 #include <string>
-#include <fstream>
 
-void getHighScores();
+void getHighScores(std::string playerArray[], int scoreArray[]);
 
 int main() {
-	getHighScores();
+	std::string players[5];
+	int scores[5];
+	getHighScores(players, scores);
 	return 0;
 }
--- players/getHighScores.cpp
+++ players/getHighScores.cpp
@@ -1,29 +1,60 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 
-void getHighScores() {
+void getHighScores(std::string playerArray[], int scoreArray[]) {
 
-	std::string players[5];
-	int scores[5];
-
+	// Initialise necessary variables and constants
+	const int NUMSCORES = 5;
+	
+	// Initialise ifstream inFile and string inLine for reading file 'scores.txt'
 	std::string inLine;
 	std::ifstream inFile("scores.TXT");
-
+	
+	// Print message if file not found, else populate arrays with lines from file
 	if (!inFile) {
-		std::cout << "Couldn't open File";
+		std::cout << "Error: File not found";
 	}
 	else {
-		std::string name;
-		int score;
+		std::string score;
 		int arrayIndex = 0;
+		// Iterates through the file, retrieving each line into their respective array
 		for (int lineNum = 0; lineNum < 10; lineNum++) {
 			getline(inFile, inLine);
 			if (lineNum % 2 == 0) {
-				players[arrayIndex]
-
+				playerArray[arrayIndex] = inLine;
 			}
+			else {
+				std::stringstream convert(inLine);
+				convert >> scoreArray[arrayIndex];
+				arrayIndex++; // increments index value for arrays ( arrays are size 5 and we are looping 10 times )
+			}
+		}
 
+		// Sorts the arrays in descending order
+		int tempScore;
+		std::string tempPlayer;
+		for (int i = 0; i < NUMSCORES - 1; i++) {
+			for (int j = i + 1; j < NUMSCORES; j++) {
+				if (scoreArray[j] > scoreArray[i]) {
+					tempScore = scoreArray[i];
+					tempPlayer = playerArray[i];
+
+					scoreArray[i] = scoreArray[j];
+					playerArray[i] = playerArray[j];
+
+					scoreArray[j] = tempScore;
+					playerArray[j] = tempPlayer;
+				}
+			}
+		}
+
+		// Print out top three players to the screen
+		std::cout << "Top three players" << std::endl;
+		for (int i = 0; i < 3; i++) {
+			std::cout << playerArray[i] << std::endl;
+			std::cout << scoreArray[i] << std::endl;
 		}
 	}
 }
